a3130d18463799dd49f9c5c687a5b982fd867296
El Hadji Ndiaye <el-hadji.ndiaye@atos.net>
2021-02-19 10:57:28 +0000

    Observable en cours

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 import { Post } from './class/post';
 import { AppareilServie } from './services/appareil.service';
 @Component({
@@ -65,8 +67,23 @@ export class AppComponent  implements OnInit {
   //   }
   // }
 
+  secondes: number;
+
   constructor(){}
 
-  ngOnInit(){}
+  ngOnInit(){
+    const counter = Observable.interval(1000);
+    counter.subscribe(
+      (value: number)=>{
+        this.secondes = value;
+      },
+      (error: any)=>{
+        console.log('Une erreur a été rencontrée !');
+      },
+      ()=>{
+        console.log('Observable complété');
+      }
+    )
+  }
 
 }
